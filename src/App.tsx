b8b9c3e7575a75efd67443b49ac2bd874254df92
Polushin Dima <dima2004polushin2004@gmail.com>
2022-11-10 14:18:55 +0600

    Описал основную логику компонентов

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import UserForm from "./UserForm/UserForm";
+import Users from "./Users/Users";
+
+import type {UsersType} from "./types";
 
 function App() {
+  const [users, setUsers] = useState<UsersType[]>([
+    {id: 'admin', name: 'Dima', email: 'lomoka@bk.ru', active: false, type: 'admin'}
+  ]);
+
+  const addUser = (user: UsersType) => {
+    setUsers(prev => [...prev, user])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='navbar navbar-expand-lg navbar-dark bg-primary mb-3'>
+        <div className="container-fluid">
+          <h3 className='text-white'>User</h3>
+        </div>
       </header>
+      <main className='container-fluid'>
+        <div className="row">
+          <div className="col">
+            <UserForm onSubmit={addUser}/>
+          </div>
+          <div className="col">
+            <Users users={users}/>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
